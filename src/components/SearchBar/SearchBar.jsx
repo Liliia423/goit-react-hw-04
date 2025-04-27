432778982b7a4aec1735fb14bb78f9ab98bdbdff
Liliia423 <etude4408@gmail.com>
2025-04-27 12:08:25 +0300

    Initial commit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,50 +1,5 @@
-{
-  /*import { useState } from "react";
-import styles from "./SearchBar.module.css";
-
-export default function SearchBar({ onSubmit }) {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleChange = (e) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const trimmedQuery = inputValue.trim();
-    if (trimmedQuery === "") {
-      onSubmit("");
-      return;
-    }
-    onSubmit(trimmedQuery);
-    setInputValue("");
-  };
-
-  return (
-    <header>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="search"
-          className={styles["search-field-input"]}
-          type="text"
-          autoComplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-          value={inputValue}
-          onChange={handleChange}
-        />
-        <button className={styles["button-fot-search-field"]} type="submit">
-          Search
-        </button>
-      </form>
-    </header>
-  );
-}*/
-}
-
 import { useState } from "react";
-import { toast } from "react-hot-toast"; // 🔥 Імпорт тостера
+import { toast } from "react-hot-toast";
 import styles from "./SearchBar.module.css";
 
 export default function SearchBar({ onSubmit }) {
